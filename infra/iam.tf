9d46ee7070337280597b841e700599f5d54eae17
Srinivas <gsry99@gmail.com>
2026-03-17 18:05:58 -0500

    codestar

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,3 +1,7 @@
+###############################################
+# SageMaker Execution Role
+###############################################
+
 resource "aws_iam_role" "sagemaker_execution_role" {
   name = "homecredit-sagemaker-execution-role"
 
@@ -59,4 +63,90 @@ resource "aws_iam_policy" "sagemaker_policy" {
 resource "aws_iam_role_policy_attachment" "attach_sagemaker_policy" {
   role       = aws_iam_role.sagemaker_execution_role.name
   policy_arn = aws_iam_policy.sagemaker_policy.arn
+}
+
+###############################################
+# CodeBuild Role
+###############################################
+
+resource "aws_iam_role" "homecredit_codebuild_role" {
+  name = "homecredit-codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "codebuild_policy" {
+  name = "homecredit-codebuild-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      # CloudWatch Logs
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      },
+
+      # S3 Access
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+          "arn:aws:s3:::${var.bucket_name}/*"
+        ]
+      },
+
+      # CodeBuild self permissions
+      {
+        Effect = "Allow"
+        Action = [
+          "codebuild:BatchGetBuilds",
+          "codebuild:StartBuild"
+        ]
+        Resource = "*"
+      },
+
+      # Allow CodeBuild to use the SageMaker role
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = aws_iam_role.sagemaker_execution_role.arn
+      },
+
+      # ⭐ REQUIRED FOR GITHUB CONNECTION ⭐
+      {
+        Effect = "Allow"
+        Action = [
+          "codeconnections:UseConnection"
+        ]
+        Resource = var.codestar_connection_arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_codebuild_policy" {
+  role       = aws_iam_role.homecredit_codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_policy.arn
 }
