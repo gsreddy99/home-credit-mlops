ed294ff1949ae5002e9e5dd27415f278d6b47566
Srinivas <gsry99@gmail.com>
2026-03-16 19:58:20 -0500

    AWS Infra

--- infra/s3.tf
+++ infra/s3.tf
@@ -1,6 +1,22 @@
 resource "aws_s3_bucket" "home_credit" {
-  bucket = var.bucket_name
+  bucket = "sg-home-credit"
+
+  tags = {
+    Name = "sg-home-credit"
+  }
 }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.home_credit.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# ---------------------------
+# Bronze / Silver / Gold prefixes
+# ---------------------------
 
 resource "aws_s3_object" "bronze_prefix" {
   bucket = aws_s3_bucket.home_credit.id
@@ -15,4 +31,18 @@ resource "aws_s3_object" "silver_prefix" {
 resource "aws_s3_object" "gold_prefix" {
   bucket = aws_s3_bucket.home_credit.id
   key    = "home-credit/gold/"
+}
+
+# ---------------------------
+# Train / Test under Bronze
+# ---------------------------
+
+resource "aws_s3_object" "bronze_train_prefix" {
+  bucket = aws_s3_bucket.home_credit.id
+  key    = "home-credit/bronze/train/"
+}
+
+resource "aws_s3_object" "bronze_test_prefix" {
+  bucket = aws_s3_bucket.home_credit.id
+  key    = "home-credit/bronze/test/"
 }
