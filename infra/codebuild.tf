c909042dfee6ae70528d68a380df23787b8ef259
Srinivas <gsry99@gmail.com>
2026-03-17 15:10:13 -0500

    Fix terraform formatting

--- infra/codebuild.tf
+++ infra/codebuild.tf
@@ -1,6 +1,6 @@
 resource "aws_codebuild_project" "homecredit" {
   name         = "homecredit-build"
-  service_role = aws_iam_role.codebuild_role.arn
+  service_role = aws_iam_role.homecredit_codebuild_role.arn
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
